bae9554a4b93d20ab0165f2aab972824b7f29f5d
scottrosoff <rosoffs96@gmail.com>
2020-06-21 18:38:24 -0700

    changed banner sizin and header lowercased

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Scott Rosoff'
-SITENAME = 'scott-rosoff'
+SITENAME = 'Scott Rosoff'
 SITEURL = 'https://scott-rosoff.github.io'
 
 PATH = 'content'
